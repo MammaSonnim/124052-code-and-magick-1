adbba7b347f5785f3134a3184d296041de52cffc
Mamma Sonnim <msonnim@gmail.com>
2017-01-25 10:41:47 +0300

    module2-task1 :ballot_box_with_check: prepared for 1st PR

--- js/stat.js
+++ js/stat.js
@@ -3,7 +3,7 @@
 window.renderStatistics = function(ctx, names, times) {
 
   /**
-   *  Конфиг для отрисовки статистики.
+   * Конфиг для отрисовки статистики.
    * @enum
    */
   var statCloudConfig = {
@@ -12,7 +12,7 @@ window.renderStatistics = function(ctx, names, times) {
       START_Y: 10,
       WIDTH: 420,
       HEIGHT: 270,
-      SKEW: 10,
+      SKEW: 5,
       SHIFT: 10,
       COLOR: '#ffffff',
       SHADOW_COLOR: 'rgba(0, 0, 0, 0.7)',
@@ -33,7 +33,10 @@ window.renderStatistics = function(ctx, names, times) {
       COL_INDENT: 50,
       MY_NAME: 'Вы',
       MY_COLOR: 'rgba(255, 0, 0, 1)',
-      BASE_COLOR: 'rgba(0, 0, 255, 1)'
+      BASE_COLOR: 'rgba(0, 0, 255, ',
+      TEXT_COLOR: 'rgba(0, 0, 0, 0.7)',
+      POS_Y: 100,
+      SHIFT: 10
     }
   };
 
@@ -59,7 +62,7 @@ window.renderStatistics = function(ctx, names, times) {
       statCloudConfig.Polygon.COLOR
     );
 
-    _drawMultiLineText(
+    _drawVerdictText(
       statCloudConfig.Polygon.START_X + statCloudConfig.Polygon.PADDING,
       statCloudConfig.Polygon.START_Y + statCloudConfig.Polygon.PADDING,
       statCloudConfig.Text.GLORY_MESSAGE,
@@ -77,14 +80,25 @@ window.renderStatistics = function(ctx, names, times) {
       statCloudConfig.Histogram.HEIGHT,
       statCloudConfig.Histogram.COL_WIDTH,
       statCloudConfig.Histogram.COL_INDENT,
-      _getHistogramStep(statCloudConfig.Histogram.HEIGHT)
+      _getHistogramStep(statCloudConfig.Histogram.HEIGHT),
+      statCloudConfig.Histogram.TEXT_COLOR,
+      statCloudConfig.Histogram.BASE_COLOR,
+      statCloudConfig.Histogram.MY_COLOR,
+      statCloudConfig.Histogram.POS_Y,
+      statCloudConfig.Histogram.SHIFT,
+      statCloudConfig.Histogram.MY_NAME
     );
   }
 
   getAllStatElements();
 
   /**
-   * Отрисовка многоугольника.
+   * @param {number} x координата левого верхнего угла
+   * @param {number} y координата левого верхнего угла
+   * @param {number} width ширина многоуголника
+   * @param {number} height высота многоуголника
+   * @param {?number} skew скос
+   * @param {string} color цвет многоуголника
    */
   function _drawPolygon(x, y, width, height, skew, color) {
     ctx.fillStyle = color;
@@ -103,9 +117,17 @@ window.renderStatistics = function(ctx, names, times) {
   }
 
   /**
-   * Отрисовка текста.
+   * @param {number} x координата левого верхнего угла
+   * @param {number} y координата левого верхнего угла
+   * @param {string} gloryMessage текст, когда пользователь победил
+   * @param {string} failureMessage текст, когда пользователь проиграл
+   * @param {string} color цвет шрифта
+   * @param {string} font стиль шрифта
+   * @param {number} lineHeight высота строки
+   * @param {string} baseline режим базовой линии
+   * @param {string} name имя пользователя
    */
-  function _drawMultiLineText(x, y, gloryMessage, failureMessage, color, font, lineHeight, baseline, name) {
+  function _drawVerdictText(x, y, gloryMessage, failureMessage, color, font, lineHeight, baseline, name) {
     ctx.fillStyle = color;
     ctx.font = font;
     ctx.baseline = baseline;
@@ -118,9 +140,20 @@ window.renderStatistics = function(ctx, names, times) {
   }
 
   /**
-   * Отрисовка гистограммы.
+   * @param {number} x координата левого верхнего угла
+   * @param {number} y координата левого верхнего угла
+   * @param {number} height максимальная высота колонки
+   * @param {number} colWidth ширина колонки
+   * @param {number} colIndent расстояние между колонками
+   * @param {number} colStep шаг роста высоты колонки
+   * @param {string} textColor цвет шрифта
+   * @param {string} colBaseColor базовый цвет для расчета цвета колонок других игроков
+   * @param {string} colMyColor цвет колонки пользователя
+   * @param {number} posY сдвиг позиционирования всех элементов гистограммы
+   * @param {number} shift отступ между элементами гистограммы
+   * @param {string} myName имя пользователя
    */
-  function _drawHistogram(x, y, height, colWidth, colIndent, colStep) {
+  function _drawHistogram(x, y, height, colWidth, colIndent, colStep, textColor, colBaseColor, colMyColor, posY, shift, myName) {
     var histogramStep = colStep;
     var colWithIndent = (colWidth + colIndent);
 
@@ -130,22 +163,55 @@ window.renderStatistics = function(ctx, names, times) {
       var colHeight = histogramStep * time;
       var colX = x + colWithIndent * i;
 
-      if (names[i] === statCloudConfig.Histogram.MY_NAME) {
-        ctx.fillStyle = statCloudConfig.Histogram.MY_COLOR;
+      ctx.fillStyle = textColor;
+
+      ctx.fillText(time.toFixed(0), colX, height - colHeight - shift + posY);
+      ctx.fillText(name, colX, y + height + shift + posY);
+
+      if (names[i] === myName) {
+        ctx.fillStyle = colMyColor;
       } else {
-        ctx.fillStyle = _getHistogramColor();
+        ctx.fillStyle = _getHistogramColor(colBaseColor);
       }
 
-      ctx.fillRect(colX, y, colWidth, colHeight);
-
-      ctx.fillText(name + ':' + time.toFixed(0), colX, y + height);
+      ctx.fillRect(colX, height - colHeight + posY, colWidth, colHeight);
     }
   }
 
+  /**
+   * @param {string} name имя пользователя
+   * @return {boolean}
+   */
   function _hasUserWon(name) {
     return times.indexOf(_getMaxResult()) === names.indexOf(name);
   }
 
+  /**
+   * @param {number} height максимальная высота колонки
+   * @return {number}
+   */
+  function _getHistogramStep(height) {
+    var max = _getMaxResult();
+    return height / max;
+  }
+
+  /**
+   * @param {string} color базовый цвет колонок других игроков
+   * @return {string}
+   */
+  function _getHistogramColor(color) {
+    var opacity = Math.random();
+
+    if (opacity < 0.5 || opacity > 1) {
+      return _getHistogramColor();
+    } else {
+      return color + opacity +  ')'
+    }
+  }
+
+  /**
+   * @return {number}
+   */
   function _getMaxResult() {
     var max = -1;
 
@@ -156,20 +222,5 @@ window.renderStatistics = function(ctx, names, times) {
       }
     }
     return max;
-  }
-
-  function _getHistogramStep(height) {
-    var max = _getMaxResult();
-    return height / max;
-  }
-
-  function _getHistogramColor() {
-    var opacity = Math.random();
-
-    if (opacity < 0.5 || opacity > 1) {
-      return _getHistogramColor();
-    } else {
-      return 'rgba(0, 0, 255, ' + opacity +  ')'
-    }
   }
 };
