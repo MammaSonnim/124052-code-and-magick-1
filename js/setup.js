6b6c0493888b0773e9e0ae860ba1dfe0e432b2ee
Mamma Sonnim <msonnim@gmail.com>
2017-02-03 00:38:00 +0300

    module4-task1 :one: first iteration

--- js/setup.js
+++ js/setup.js
@@ -4,6 +4,7 @@ var setup = document.querySelector('.setup');
 var setupOpenBtn = document.querySelector('.setup-open');
 var setupForm = document.querySelector('.setup-wizard-form');
 var setupCloseBtn = setupForm.querySelector('.setup-close');
+var setupSubmitBtn = setupForm.querySelector('.setup-submit');
 var setupUserName = setupForm.querySelector('.setup-user-name');
 var wizard = setupForm.querySelector('#wizard');
 var wizardCoat = wizard.querySelector('#wizard-coat');
@@ -13,6 +14,8 @@ var CSS_CLASS_INVISIBLE = 'invisible';
 var USER_NAME_MAXLENGTH = '50';
 var ARIA_CURRENT_VALUE_ATTRIBUTE = 'aria-valuenow';
 var ARIA_PRESSED_ATTRIBUTE = 'aria-pressed';
+var ENTER_KEY_CODE = 13;
+var ESCAPE_KEY_CODE = 27;
 
 var colors = {
   WIZARD_COAT: [
@@ -39,7 +42,37 @@ var colors = {
   ]
 };
 
-// wizard click handlers
+
+// modal btns event handlers
+var setupOpenBtnClickHandler = function (event) {
+  open();
+};
+var setupOpenBtnKeydownHandler = function (event) {
+  if (event.keyCode === ENTER_KEY_CODE) {
+    open();
+  }
+};
+var setupCloseBtnClickHandler = function (event) {
+  close();
+};
+var setupCloseBtnKeydownHandler = function (event) {
+  if (event.keyCode === ENTER_KEY_CODE) {
+    close();
+  }
+};
+var setupSubmitBtnKeydownHandler = function (event) {
+  if (event.keyCode === ENTER_KEY_CODE) {
+    event.preventDefault();
+    close();
+  }
+};
+var documentKeydownHandler = function (event) {
+  if (event.keyCode === ESCAPE_KEY_CODE) {
+    close();
+  }
+};
+
+// wizard parts event handlers
 var wizardCoatClickHandler = function (event) {
   paintElementsWithRandomColor(event, colors.WIZARD_COAT, 'fill');
 };
@@ -50,9 +83,17 @@ var fireballClickHandler = function (event) {
   paintElementsWithRandomColor(event, colors.FIREBALL, 'background');
 };
 
+setupOpenBtn.addEventListener('click', setupOpenBtnClickHandler);
+setupOpenBtn.addEventListener('keydown', setupOpenBtnKeydownHandler);
+
 // business logic
 function open() {
   toggleState(true);
+
+  setupCloseBtn.addEventListener('click', setupCloseBtnClickHandler);
+  setupCloseBtn.addEventListener('keydown', setupCloseBtnKeydownHandler);
+  document.addEventListener('keydown', documentKeydownHandler);
+  setupSubmitBtn.addEventListener('keydown', setupSubmitBtnKeydownHandler);
 
   wizardCoat.addEventListener('click', wizardCoatClickHandler);
   wizardEyes.addEventListener('click', wizardEyesClickHandler);
@@ -64,11 +105,20 @@ function close() {
   wizardEyes.removeEventListener('click', wizardEyesClickHandler);
   fireball.removeEventListener('click', fireballClickHandler);
 
+  setupCloseBtn.removeEventListener('click', setupCloseBtnClickHandler);
+  setupCloseBtn.removeEventListener('keydown', setupCloseBtnKeydownHandler);
+  document.removeEventListener('keydown', documentKeydownHandler);
+  setupSubmitBtn.removeEventListener('keydown', setupSubmitBtnKeydownHandler);
+
   toggleState(false);
 }
 
 function toggleState(isOpened) {
-  setup.classList.toggle(CSS_CLASS_INVISIBLE);
+  if (isOpened) {
+    setup.classList.remove(CSS_CLASS_INVISIBLE);
+  } else {
+    setup.classList.add(CSS_CLASS_INVISIBLE);
+  }
   setupUserName.required = isOpened;
   setupUserName.maxlength = isOpened ? USER_NAME_MAXLENGTH : false;
   setupOpenBtn.setAttribute(ARIA_PRESSED_ATTRIBUTE, isOpened.toString());
@@ -84,6 +134,3 @@ function paintElementsWithRandomColor(event, colorArray, paintMethod) {
 function getRandomElement(array) {
   return array[Math.floor(Math.random() * array.length)];
 }
-
-setupOpenBtn.addEventListener('click', open);
-setupCloseBtn.addEventListener('click', close);
